40b21c89a4b13abca5cd07098e0758df38e08f2f
Raghuveera Betina <rbetina@gmail.com>
2011-12-12 14:22:22 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DealmeIn::Application.config.secret_token = '2242cf50f6a6f82dbf4d5d91c7dba80215abda9d86a696b72bdf0f9cf21069f92371f8dbc8c6d933d5466e9fe393a1a3e5cc365065b5a5f642072e66ee40f746'
